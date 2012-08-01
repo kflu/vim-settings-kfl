866f36c0fe6de209c3c674be8db62330032b8237
Kefei Lu <lukefei@gmail.com>
2012-07-31 23:06:03 -0700

    Initial checking in.

---
+++ COPY_TO_HOME/vimfiles/bundle/README.txt
@@ -0,0 +1,2 @@
+Plugins/syntax files/... go into this folder and will be automatically loaded.
+To disable a plugin, just drop the plugin folder into Bundle_disabled
---
+++ install.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+set vim=%~dp0
+
+pushd %userprofile%
+echo Linking files from %vim%\COPY_TO_HOME to %userprofile%...
+:: Making hardlinks for files because of the vim-WinSymLink bug
+mklink /h _vimrc %vim%\COPY_TO_HOME\_vimrc
+mklink /d vimfiles %vim%\COPY_TO_HOME\vimfiles
+
+pushd c:\windows
+echo Linking files from %vim%\COPY_TO_HOME to c:\windows...
+:: Don't make hardlink here as anywhere it's less likely that we edit files
+:: here
+for %%i in (%vim%\COPY_TO_PATH\*) do (
+	mklink %%~nxi %%i
+)
+popd
+
+popd
