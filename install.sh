fe1547d9ef69f9197965348ed2e36cc0939fb32d
KFL <kfldev@outlook.com>
2015-08-05 22:37:58 -0700

    modified linux setup file

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 SCRIPT=`readlink -f $0`
 DIR=`dirname $SCRIPT`
 ln -s -f "$DIR"/COPY_TO_HOME/_vimrc ~/.vimrc
-ln -s -f "$DIR"/COPY_TO_HOME/vimfiles ~/.vim
+
+cd ~
+mkdir vundle
+pushd vundle
+git clone https://github.com/gmarik/Vundle.vim.git
+popd
