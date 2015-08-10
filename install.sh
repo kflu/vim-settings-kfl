3f27ffc1669ad026217100597bb08346536d331b
KFL <kfldev@outlook.com>
2015-08-09 18:10:23 -0700

    fix install.sh. added shebang

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
+
 SCRIPT=`readlink -f $0`
 DIR=`dirname $SCRIPT`
 ln -s -f "$DIR"/COPY_TO_HOME/_vimrc ~/.vimrc
 
-cd ~
-mkdir vundle
-pushd vundle
+mkdir ~/vundle
+pushd ~/vundle
 git clone https://github.com/gmarik/Vundle.vim.git
 popd
