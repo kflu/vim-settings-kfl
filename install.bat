415e47be0aa377f3955dc73e1cdbee73eeb42c1e
KFL <kfldev@outlook.com>
2015-08-09 22:45:59 -0700

    fixed install.bat

--- install.bat
+++ install.bat
@@ -8,7 +8,7 @@ mklink _vimrc %vim%\COPY_TO_HOME\_vimrc
 mklink .ctags %vim%\COPY_TO_HOME\.ctags
 
 pushd c:\windows
-echo Linking files from %vim%\COPY_TO_HOME to c:\windows...
+echo Linking files from %vim%\COPY_TO_PATH to c:\windows...
 :: Don't make hardlink here as anywhere it's less likely that we edit files
 :: here
 for %%i in (%vim%\COPY_TO_PATH\*) do (
