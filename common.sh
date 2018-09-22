af03dcaa883287376dfbe8536c1158cb897603cc
KFL <kfldev@outlook.com>
2018-09-21 21:13:23 -0700

    .

--- common.sh
+++ common.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
+
 source_rc() {
     dir="$1"
     pattern="$2"
-    printf "sourcing $dir/$pattern\n"
-    find "$dir" -type f -name "$pattern" | while read fn
-    do 
-        printf "sourcing $fn" 
-        source $fn
+    find "$dir" -type f -o -type l -name "$pattern" | while read fn; do 
+        case $fn in
+            (*~) ;;
+            (.*~) ;;
+            (*) 
+                printf "sourcing $fn\n"
+                source $fn
+                ;;
+        esac
     done
 }
