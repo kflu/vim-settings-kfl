b6aa714f172ee3b37e97a88f03f8122e95b98489
KFL <kfldev@outlook.com>
2018-09-22 00:57:40 -0700

    .

---
+++ install_fluxbox.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+yum install fluxbox \
+            gmrun \
+            rxvt-unicode-256color \
+            xterm \
+            chromium-browser \
+            xclip \
+            xpdf \
