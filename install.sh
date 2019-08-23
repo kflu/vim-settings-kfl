498c9fa35d5fef2980ff5921ca300edb528f43f3
KFL <kludev@gmail.com>
2019-08-23 18:26:43 +0000

    remove fluxbox settings => migrated to fluxbox-settings-kfl

--- install.sh
+++ install.sh
@@ -7,22 +7,6 @@ ln -v -s -f $DIR/.Xresources ~/.Xresources
 ln -v -s -f $DIR/.ideavimrc ~/.ideavimrc
 touch ~/.Xresources.dpi
 
-mkdir -v -p ~/.fluxbox
-
-# cp $DIR/fluxbox/_init ~/.fluxbox/init
-# copy the base init over: only set default style
-echo "session.styleFile:	$HOME/.fluxbox/styles/Dyne" > ~/.fluxbox/init
-
-ln -v -s -f $DIR/fluxbox/keys ~/.fluxbox/keys
-ln -v -s -f $DIR/fluxbox/menu ~/.fluxbox/menu
-ln -v -s -f $DIR/fluxbox/apps ~/.fluxbox/apps
-ln -v -s -f $DIR/fluxbox/startup ~/.fluxbox/startup
-ln -v -s -f $DIR/fluxbox/windowmenu ~/.fluxbox/windowmenu
-ln -v -s -f -t ~/.fluxbox $DIR/fluxbox/styles
-
-cat $DIR/fluxbox/styles/startrek.tar.gz | tar xzO > $DIR/fluxbox/styles/startrek.gitignore.xpm
-ln -v -s -f $DIR/fluxbox/styles/startrek.gitignore.xpm $DIR/fluxbox/styles/Dyne/pixmaps/startrek.gitignore.xpm
-
 mkdir -v -p ~/.fonts
 ln -v -s -f $DIR/.fonts/FreeSans.ttf ~/.fonts/FreeSans.ttf
 
--- install_fluxbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-yum install fluxbox \
-            gmrun \
-            rxvt-unicode-256color \
-            xterm \
-            chromium-browser \
-            xclip \
-            xpdf \
