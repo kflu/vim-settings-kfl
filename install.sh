2cfa531eadc9aa1b568d790a4de6a909838967e9
KFL <kludev@gmail.com>
2020-09-06 23:47:24 -0700

    restructured directory structure and update install

--- install.sh
+++ install.sh
@@ -2,32 +2,32 @@
 
 DIR="$( cd "`dirname "$0"`" && pwd )"
 
-ln -s -f $DIR/.screenrc ~/.screenrc
-ln -s -f $DIR/.gitconfig ~/.gitconfig
-ln -s -f $DIR/.Xclients ~/.Xclients
-ln -s -f $DIR/.Xresources ~/.Xresources
-ln -s -f $DIR/.ideavimrc ~/.ideavimrc
+err() {
+    echo "$1" 2>&1
+    exit 1
+}
+
+has_git() {
+    git --version 2>&1 > /dev/null
+}
+
+# Install files under in $1/ to $2/ via symlink
+install_links() (
+    local src="$1"; shift
+    local dest="$1"; shift
+
+    cd "$src"
+    find . -type f | while read fn; do
+        local ln_src="`pwd`/$fn"
+        local ln_dest="$dest/$fn"
+        mkdir -p "$( dirname "$ln_dest" )"
+        echo "$ln_src => $ln_dest"
+        ln -s -f "$ln_src" "$ln_dest"
+    done
+) 
+
+install_links "$DIR/home" "$HOME"
 touch ~/.Xresources.dpi
-
-mkdir -v -p ~/.fonts
-ln -s -f $DIR/.fonts/FreeSans.ttf ~/.fonts/FreeSans.ttf
-
-mkdir -v -p ~/.urxvt/ext
-ln -s -f $DIR/.urxvt/ext/font-size ~/.urxvt/ext/font-size
-
-mkdir -v -p ~/install/share/bin
-find "$DIR/install/share/bin" -type f '!' -name '*~' | while read fn; do
-    target="`basename $fn`"
-    ln -s -f "$fn" "$HOME/install/share/bin/$target"
-done
-
-mkdir -v -p ~/install/share/man
-# install markdown docs
-find "$DIR/install/share/man" -type f '!' -name '*~' | while read fn; do
-    target="`basename $fn`"
-    ln -s -f "$fn" "$HOME/install/share/man/$target"
-done
-
 
 if [ ! -f ~/.profile ] || ! grep -q "profile.mine" ~/.profile; then
     printf "\n. $DIR/profile.mine\n" >> ~/.profile
@@ -49,31 +49,25 @@ fi
 # ADDITIONAL PACKAGES
 # ---------
 
-# setup git
-git --version 2>&1 > /dev/null
-GIT_FOUND=$?
-if [ "$GIT_FOUND" -ne 0 ]; then
-    if [ `uname` -eq "Linux" ]; then
-        apt-get update && apt-get install git
+( # --- set up tmux ---
+    has_git || err "[err] git not found: cannot set up tmux"
+    cd $HOME
+    if ! [ -d ~/.tmux ]; then
+        git clone https://github.com/kflu/.tmux.git
     fi
-fi
+    ln -s -f .tmux/.tmux.conf "$HOME/.tmux.conf"
+    cp "$HOME/.tmux/.tmux.conf.local" "$HOME"
+    printf "\n" >> "$HOME/.tmux.conf.local"
+    printf "set -g mouse on\n" >> "$HOME/.tmux.conf.local"
+)
 
-# setup tmux
-oldpath=`pwd`; cd $HOME
-if ! [ -d ~/.tmux ]; then
-    git clone https://github.com/kflu/.tmux.git
-fi
-ln -s -f .tmux/.tmux.conf ~/.tmux.conf
-cp ~/.tmux/.tmux.conf.local ~
-printf "\n"
-printf "set -g mouse on\n" >> ~/.tmux.conf.local
-
-mkdir -v -p ~/rc.d/sh/pre \
-         ~/rc.d/sh/post \
-         ~/rc.d/bash/post \
-         ~/rc.d/bash/pre \
-         ~/rc.d/zsh/post \
-         ~/rc.d/zsh/pre
+mkdir -v -p \
+    "$HOME/rc.d/sh/pre" \
+    "$HOME/rc.d/sh/post" \
+    "$HOME/rc.d/bash/post" \
+    "$HOME/rc.d/bash/pre" \
+    "$HOME/rc.d/zsh/post" \
+    "$HOME/rc.d/zsh/pre"
 
 # --- Clipboard ----
 # This works for v2.7 but not v2.3. I can't find a way to support x11 copy across versions.
@@ -81,11 +75,10 @@ mkdir -v -p ~/rc.d/sh/pre \
 # printf 'bind -Tcopy-mode-vi M-y send -X copy-pipe "xclip -i -sel p -f | xclip -i -sel c" \; display-message "copied to system clipboard"' >> ~/.tmux.conf.local
 # printf "\n" >> ~/.tmux.conf.local
 
-# --- youtube-vlc ----
-if [ ! -e ~/.youtube-vlc ]; then
-    git clone https://github.com/kflu/youtube-vlc.git ~/.youtube-vlc
-fi
-ln -s -f ~/.youtube-vlc/bin/youtube-vlc ~/install/share/bin/youtube-vlc
-
-cd $oldpath
-
+( # --- youtube-vlc ----
+    has_git || err "[err] git not found: cannot set up youtube-vlc"
+    if [ ! -e ~/.youtube-vlc ]; then
+        git clone https://github.com/kflu/youtube-vlc.git ~/.youtube-vlc
+    fi
+    ln -s -f ~/.youtube-vlc/bin/youtube-vlc ~/install/share/bin/youtube-vlc
+)
