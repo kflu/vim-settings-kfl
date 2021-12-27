407676a803fa42274fa15aa2c5d453c5c197aa62
KFL <kludev@gmail.com>
2021-12-26 20:50:27 -0800

    _

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 DIR="$( cd "`dirname "$0"`"; pwd )"
 ln -s -f "$DIR"/COPY_TO_HOME/_vimrc "$HOME/.vimrc"
 
-# cloning vim-plug repo into `autoload` folder because vim-plug repo has 
-# `plug.vim` in the repo's root (this is different than Vundle)
-mkdir ~/vim-plug/vim-plug
-git clone https://github.com/junegunn/vim-plug.git ~/vim-plug/vim-plug/autoload
+# ~/vim-plug is where we put all the plugins managed by vim-plug
+# However vim-plug itself is installed into ~/.vim/autoload
+mkdir "$HOME/vim-plug"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
