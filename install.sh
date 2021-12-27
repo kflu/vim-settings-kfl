46701ff25fe7e421a0cff302d0126b1e81143151
KFL <kludev@gmail.com>
2021-12-26 21:15:25 -0800

    _

--- install.sh
+++ install.sh
@@ -6,5 +6,15 @@ ln -s -f "$DIR"/COPY_TO_HOME/_vimrc "$HOME/.vimrc"
 # ~/vim-plug is where we put all the plugins managed by vim-plug
 # However vim-plug itself is installed into ~/.vim/autoload
 mkdir "$HOME/vim-plug"
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+curl -fLo ~/.vim/doc/plug.txt --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/doc/plug.txt
+
+# build helptags for vim-plug
+vim -es - <<'EOF'
+helptags ~/.vim/doc
+qa
+EOF
