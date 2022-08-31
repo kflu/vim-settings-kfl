de227d298f243a59f1dc3ece7db8cd53a14b3120
KFL <kludev@gmail.com>
2022-08-30 23:28:44 -0700

    fix install.sh for windows

--- install.sh
+++ install.sh
@@ -1,20 +1,34 @@
 #!/bin/sh
+set -e
 
 DIR="$( cd "`dirname "$0"`"; pwd )"
-ln -s -f "$DIR"/COPY_TO_HOME/_vimrc "$HOME/.vimrc"
+case "$(uname -s)" in
+    *NT*)
+        vimrc="$HOME/_vimrc"
+        vimfiles="$HOME/vimfiles"
+        ;;
+    *)
+        vimrc="$HOME/.vimrc"
+        vimfiles="$HOME/.vim"
+        ;;
+esac
 
+ln -s -f "$DIR"/COPY_TO_HOME/_vimrc "$vimrc"
+mkdir -p "$vimfiles"
 # ~/vim-plug is where we put all the plugins managed by vim-plug
 # However vim-plug itself is installed into ~/.vim/autoload
-mkdir "$HOME/vim-plug"
+mkdir -p "$HOME/vim-plug"
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+mkdir -p "$vimfiles/autoload" && \
+curl -fLo "$vimfiles/autoload/plug.vim" --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-curl -fLo ~/.vim/doc/plug.txt --create-dirs \
+mkdir -p "$vimfiles/doc" && \
+curl -fLo "$vimfiles/doc/plug.txt" --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/doc/plug.txt
 
 # build helptags for vim-plug
-vim -es - <<'EOF'
-helptags ~/.vim/doc
+vim -es - <<EOF
+helptags $HOME/$vimfiles/doc
 qa
 EOF
